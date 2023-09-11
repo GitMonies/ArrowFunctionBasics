1a25ee17ab237757d50d3482715ae0def87b8fbd
GitMonies <jarod.smelosky@yahoo.com>
2023-09-11 08:17:35 -0500

    first commit

---
+++ index.js
@@ -0,0 +1,18 @@
+
+// 1. Product of two numbers using an arrow function.
+
+// let product = (a,b) => a * b;
+
+// alert(product(3,4));
+
+// 2. String to Uppercase using an arrow function.
+
+// let conversion = (sandwich) => {return sandwich.toUpperCase()};
+
+
+// alert(conversion("michael"))
+
+// 3. Square of a single number using an arrow function.
+
+let square = n => n * n;
+alert(square(3))
